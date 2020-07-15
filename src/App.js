8314deafaa47bfdad159a074bbe60c7757978605
Harshad <harsh.haou@gmail.com>
2020-07-15 15:58:46 +0530

    messeges

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React, { Component } from 'react';
+import React,{ useState, useEffect } from "react"
+import { Button,InputLabel,FormControl,Input } from '@material-ui/core';
+import Message from "./Message";
+
+const App = () => {
+    const [input, setInput] = useState('');
+    const [user, setUser] = useState('pro Programer');
+    const [messages, setMesseges] = useState([{azi: 'hi there', sara: 'hello', azi: 'woww'}]);
+    
 
 
-class App extends Component {
-    render() {
-        return (
-            <div>hii</div>
-        );
+    let addMessage = (e) => {
+        e.preventDefault()
+        setMesseges([...messages, input])
+        setInput('')
     }
+
+    useEffect(() => {
+        setUser(prompt('Welcome pro Programer'))
+    },[]);
+
+    let renderMessages  = () => {
+    return messages.map((message, index) => <Message key={index} message={message} />)
+    }
+
+    return (
+        <div className='app'>
+            <h1>Chat app</h1>
+            <h2>Welcome {user}</h2>
+            <form>
+            <FormControl>
+            <InputLabel htmlFor="my-input">Type message... </InputLabel>
+            <Input onChange={(e) => setInput(e.target.value)} id="my-input" aria-describedby="my-helper-text" value={input} type='text' />
+            <Button disabled={!input} type='submit' color="secondary" onClick={addMessage}>send message</Button>
+            </FormControl>
+            </form>
+            <div> {renderMessages()} </div>
+
+        </div> 
+    );
 }
 
 export default App;
---
+++ src/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Message = ({message}) => {
+    return (
+        <div className='message-list'>
+            {message}
+        </div>
+    );
+}
+
+export default Message;
